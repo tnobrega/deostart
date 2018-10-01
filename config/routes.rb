8743cdca5ba6c2bb6bf5930fb408378561b2ae32
thiago nobrega <tnobrega@gmail.com>
2018-10-01 16:23:01 -0300

    Content

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   root to: 'posts#index'
+
+  get '/conteudo_extra', to: 'pages#content', as: 'content'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
