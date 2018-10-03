9df23ae128a818c92250e39b41e2fd5fd48aef79
thiago nobrega <tnobrega@gmail.com>
2018-10-03 13:47:49 -0300

    email validation

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,5 +1,5 @@
 class Lead < ApplicationRecord
 	validates :name, presence: { message: "O Nome é obrigatório" }
-	validates :last_name, presence: { message: "O Sobrenome é obrigatório" }
-	validates :email, presence: true, uniqueness: { message: "Esse e-mail já foi cadastrado" }
+	validates :last_name, presence: { message: "O Sobrenome é obrigatório" } 
+	validates :email, presence: true, uniqueness: { message: "Esse e-mail já foi cadastrado" }, format: { with: URI::MailTo::EMAIL_REGEXP, message: " com formato inválido" }
 end
