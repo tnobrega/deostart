e6c656e6bc559a633977148cb32bd9ff012466d8
thiago nobrega <tnobrega@gmail.com>
2018-10-05 13:50:19 -0300

    privacy_policy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :content]
+  skip_before_action :authenticate_user!, only: [:home, :content, :privacy_policy]
   def home
   end
 
   def content
+  end
+
+  def privacy_policy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/conteudo_extra', to: 'pages#content', as: 'content'
   get '/show_me_the_leads', to: 'leads#index', as: 'show_me_the_leads'
+  get '/politica_privacidade', to: 'pages#privacy_policy', as: 'privacy_policy'
   get '/:id' , to: 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
