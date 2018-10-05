f62b4d56e528de9e7970c0616d65867d6068aed9
thiago nobrega <tnobrega@gmail.com>
2018-10-05 09:26:04 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
 
-  get '/:id' , to: 'posts#show'
 
   get '/conteudo_extra', to: 'pages#content', as: 'content'
   get '/show_me_the_leads', to: 'leads#index', as: 'show_me_the_leads'
+  get '/:id' , to: 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
