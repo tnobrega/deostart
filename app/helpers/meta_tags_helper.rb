c99e7a4630937b14c1d7ac4b5fb78a9f751579d9
thiago nobrega <tnobrega@gmail.com>
2018-10-05 03:06:36 -0300

    meta title

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -4,4 +4,8 @@ module MetaTagsHelper
     content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
 
+  def meta_title
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
+  end
+
 end
